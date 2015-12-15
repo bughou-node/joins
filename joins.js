737eed37115369c7fb98f488bcef4af2eefd156e
zhiliang hou <bughou@gmail.com>
2015-12-15 20:57:06 +0800

    save

---
+++ joins.js
@@ -0,0 +1,91 @@
+
+exports.keys = get_keys;
+exports.left = left_join;
+exports.inner = inner_join;
+
+function left_join (left, right, path) {
+  return join(left, right, path, true);
+}
+
+function inner_join (left, right, path) {
+  return join(left, right, path);
+}
+
+function join (left, right, path, flag) {
+  if (left.constructor !== Array) throw 'left must be an Array';
+  if (right.constructor !== Array) throw 'right must be an Array';
+  if (
+    !path || path.right === undefined || path.right === null
+  ) throw 'path.right is required';
+
+  var hash = { };
+  var rp = get_path(path, 'right');
+  for (var i = 0, len = right.length; i < len; i++) {
+    var r = right[i];
+    hash[get(r, rp)] = r;
+  }
+
+  var lp = get_path(path, 'left');
+  var tp = get_path(path, 'target');
+
+  var result = [ ];
+  for (var i = 0, len = left.length; i < len; i++) {
+    var l = left[i];
+    var r = hash[get(l, lp)];
+    if (flag || r !== undefined) result.push(merge(l, r, tp));
+  }
+  return result;
+}
+
+function get_keys (array, path) {
+  var keys = [ ];
+  if (path) path = path.split('.');
+  for (var i = 0, l = array.length; i < l; i++) {
+    keys.push(get(array[i], path));
+  }
+  return keys;
+}
+
+function merge (left, right, path) {
+  if (right === undefined) return left;
+  if (typeof(left) !== 'object') return right;
+  if (path) {
+    set(left, path, right);
+  } else {
+    if (left.constructor === Array && right.constructor === Array) {
+      for (var i = 0, len = right.length; i < len; i++) left.push(right[i]);
+    } else {
+      for (var k in right) left[k] = right[k];
+    }
+  }
+  return left;
+}
+
+function get_path (path, which) {
+  var p = path[which];
+  if (p === undefined || p === null) return;
+  var cons = p.constructor;
+  if (cons === String) return p.split('.');
+  else if (cons === Number) return [ p ];
+  else throw 'path.' + which + ' can only be integer or string';
+}
+
+
+function get (object, path) {
+  if (path === undefined || path === null) return object;
+  for (var i = 0, len = path.length; i < len; i++) {
+    if (!object) return;
+    object = object[path[i]];
+  }
+  return object;
+}
+
+function set (object, path, value) {
+  var last = path.length - 1;
+  for (var i = 0; i < last; i++) {
+    var p = path[i];
+    if (object[p] === undefined) object[p] = { };
+    object = object[p];
+  }
+  object[path[last]] = value;
+}
---
+++ test.js
@@ -0,0 +1,235 @@
+var assert = require("assert");
+var equal = require("deep-equal");
+
+var left_join = require('./joins.js').left;
+var inner_join = require('./joins.js').inner;
+var get_keys = require('./joins.js').keys;
+
+describe('left_join', function() {
+  it('integers left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push(i);
+      expect.push(i);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push(i);
+      right.push({ id: i, name: 'x' + i });
+      expect.push({ id: i, name: 'x' + i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ id: i, name: 'x' + i });
+    }
+    var result = left_join(left, right, { right: 'id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('strings left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push('n' + i);
+      expect.push('n' + i);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push('n' + i);
+      right.push({ id: 'n' + i, name: 'x' + i });
+      expect.push({ id: 'n' + i, name: 'x' + i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ id: i, name: 'x' + i });
+    }
+    var result = left_join(left, right, { right: 'id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('arrays left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push(['a' + i, i, 'b' + i]);
+      expect.push(['a' + i, i, 'b' + i]);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push(['a' + i, i, 'b' + i]);
+      right.push([ 'c' + i, i, 'd' + i ]);
+      expect.push(['a' + i, i, 'b' + i, 'c' + i, i, 'd' + i ]);
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push([ 'c' + i, i, 'd' + i ]);
+    }
+    var result = left_join(left, right, { left: 1, right: 1 });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('objects left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push({   id: i, a: i, b: i });
+      expect.push({ id: i, a: i, b: i });
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push({   id: i, a: i, b: i });
+      right.push({ r: { id: i }, c: i, d: i });
+      expect.push({ id: i, a: i, b: i, r: { id: i }, c: i, d: i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ rid: i, c: i, d: i });
+    }
+    var result = left_join(left, right, { left: 'id', right: 'r.id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('target path', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push({   id: i, a: i, b: i });
+      expect.push({ id: i, a: i, b: i });
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push({   id: i, a: i, b: i });
+      right.push({ r: { id: i }, c: i, d: i });
+      expect.push({ id: i, a: i, b: i, right: {
+        target: { r: { id: i }, c: i, d: i }
+      }});
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ rid: i, c: i, d: i });
+    }
+    var result = left_join(left, right, {
+      left: 'id', right: 'r.id', target: 'right.target'
+    });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+});
+
+describe('inner_join', function() {
+  it('integers left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push(i);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push(i);
+      right.push({ id: i, name: 'x' + i });
+      expect.push({ id: i, name: 'x' + i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ id: i, name: 'x' + i });
+    }
+    var result = inner_join(left, right, { right: 'id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('strings left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push('n' + i);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push('n' + i);
+      right.push({ id: 'n' + i, name: 'x' + i });
+      expect.push({ id: 'n' + i, name: 'x' + i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ id: i, name: 'x' + i });
+    }
+    var result = inner_join(left, right, { right: 'id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('arrays left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push(['a' + i, i, 'b' + i]);
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push(['a' + i, i, 'b' + i]);
+      right.push([ 'c' + i, i, 'd' + i ]);
+      expect.push(['a' + i, i, 'b' + i, 'c' + i, i, 'd' + i ]);
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push([ 'c' + i, i, 'd' + i ]);
+    }
+    var result = inner_join(left, right, { left: 1, right: 1 });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('objects left', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push({   id: i, a: i, b: i });
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push({   id: i, a: i, b: i });
+      right.push({ r: { id: i }, c: i, d: i });
+      expect.push({ id: i, a: i, b: i, r: { id: i }, c: i, d: i });
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ r: { id: i }, c: i, d: i });
+    }
+    var result = inner_join(left, right, { left: 'id', right: 'r.id' });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+
+  it('target path', function() {
+    var left = [ ], right = [ ], expect = [ ];
+
+    for (var i = 0; i < 3; i++) {
+      left.push({   id: i, a: i, b: i });
+    }
+    for (var i = 3; i < 6; i++) {
+      left.push({   id: i, a: i, b: i });
+      right.push({ r: { id: i }, c: i, d: i });
+      expect.push({ id: i, a: i, b: i, right: {
+        target: { r: { id: i }, c: i, d: i }
+      }});
+    }
+    for (var i = 6; i < 9; i++) {
+      right.push({ r: { id: i }, c: i, d: i });
+    }
+    var result = inner_join(left, right, {
+      left: 'id', right: 'r.id', target: 'right.target'
+    });
+    debug(left, right, expect, result);
+
+    assert(equal(result, expect));
+  });
+});
+
+function debug(left, right, expect, result) {
+  if (!process.env.debug) return;
+  console.log(left);
+  console.log();
+  console.log(right);
+  console.log();
+  console.log(expect);
+  console.log();
+  console.log(result);
+}
+
